ae23b83d887be0b7798b505e5603c52858fc5e81
Will Ross <paxswill@gmail.com>
2012-09-25 22:33:11 -0400

    Initial commit, stubbed out functions

---
+++ profound-wallapapers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+def get_top(feed_url):
+    pass
+
+def pick_image(top_item):
+    pass
+
+def set_background(image_url):
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
